0197bdddd633d0c5ef548eeb8b7b525bfdddc48d
Missisol <missisololol@gmail.com>
2025-04-26 02:07:07 +0300

    added charts

--- app/static/js/chart-history.js
+++ app/static/js/chart-history.js
@@ -1,5 +1,10 @@
 // apexcharts https://apexcharts.com/docs/creating-first-javascript-chart/
 
+const params = [
+  ['#chartT', 'temperature', 'температура', ['#1d531c', '#3ba639']],
+  ['#chartH', 'humidity', 'влажность', ['#1b4a79', '#3392f1']],
+  ['#chartP', 'pressure', 'давление', ['#6e3889', '#cc66ff']],
+]
 
 async function getHistoryData() {
   const response = await fetch('/json_history')
@@ -11,7 +16,6 @@ function getChartDataFromDbData(arr, parameter) {
   const categoriesValues = []
   const minValues = []
   const maxValues = []
-  let names = []
   
   arr.map((item) => {
     const date = new Date(item.date).toLocaleDateString('ru')
@@ -20,44 +24,67 @@ function getChartDataFromDbData(arr, parameter) {
     const edge = parameter !== 'pressure' ? 100 : 800
     const maxV = item[`max_${parameter}`] >= edge ? item[`min_${parameter}`] : item[`max_${parameter}`]
     maxValues.push(maxV)
-    names = [`min_${parameter}`, `max_${parameter}`]
   })
-  return {categoriesValues, minValues, maxValues, names}
+  return {categoriesValues, minValues, maxValues}
 }
 
-function getOptionsForChart(dbData, parameter) {
-  const {categoriesValues, minValues, maxValues, names } = getChartDataFromDbData(dbData, parameter)
+function getOptionsForChart(dbData, parameter, title, colors) {
+  const {categoriesValues, minValues, maxValues } = getChartDataFromDbData(dbData, parameter)
+  const titleText = title.toUpperCase()
+  const names = [`мин ${title}`, `макс ${title}`]
 
   return {
+    title: {
+      text: titleText,
+      align: 'center',
+      style: {
+        fontSize: '20px',
+        fontFamily: 'system-ui, sans-serif',  
+        color: colors[1],
+      }
+    },
     chart: {
-      type: 'line'
+      type: 'line',
+      height: 350
     },
     series: [
       {
-      name: names[0],
-      data: minValues
-    },
+        name: names[0],
+        data: minValues,
+        color: colors[0],
+      },
       {
-      name: names[1],
-      data: maxValues
+        name: names[1],
+        data: maxValues,
+        color: colors[1],
+      },
+    ],
+    stroke: {
+      dashArray: [0, 4],
     },
-  ],
     xaxis: {
-      categories: categoriesValues
-    }
+      categories: categoriesValues,
+    },
+    legend: {
+      labels: {
+        colors: ['#b2b0b0', '#b2b0b0']
+      }
+    },
   }
 }
 
-function renderChart(selector, parameter, dbData) {
-  const options = getOptionsForChart(dbData, parameter)
+function renderChart(dbData, selector, parameter, title, colors) {
+  const options = getOptionsForChart(dbData, parameter, title, colors)
   const chart = new ApexCharts(document.querySelector(selector), options)
   chart.render()
 }
 
 const res = await getHistoryData()
 
-renderChart('#chartT', 'temperature', res)
-renderChart('#chartH', 'humidity', res)
-renderChart('#chartP', 'pressure', res)
+function initCharts() {
+  params.forEach(param => renderChart(res, ...param))
+}
+
+initCharts()
 
 
