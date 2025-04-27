57ecb2840bb6634acd520fbf579666684ef786a3
Missisol <missisololol@gmail.com>
2025-04-27 23:30:46 +0300

    header menu updated

--- app/static/js/header.js
+++ app/static/js/header.js
@@ -2,6 +2,9 @@ const pathname = document.location.pathname
 const href = document.location.pathname
 const items = Array.from(document.querySelectorAll('.nav__item'))
 
+const nested = document.querySelector('#menu-dropdown')
+const button = document.querySelector('.nav__button')
+
 items.forEach((item) => {
     const a = item.dataset.url.replace('sensor.', '/')
     if (a === href) {
@@ -11,4 +14,10 @@ items.forEach((item) => {
     }
 })
 
-
+document.body.addEventListener('click', (e) => {
+    if (nested.classList.contains('open')) {
+        nested.classList.remove('open')
+    } else if (e.target === button && !nested.classList.contains('open')) {
+        nested.classList.add('open')
+    }
+})
