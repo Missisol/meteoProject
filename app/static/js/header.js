b1967201f1318770343e0ade3fbd0e0ca041bcbf
Missisol <missisololol@gmail.com>
2025-04-30 17:41:54 +0300

    added color theme switcher

--- app/static/js/header.js
+++ app/static/js/header.js
@@ -2,6 +2,9 @@ const pathname = document.location.pathname
 const href = document.location.pathname
 const items = Array.from(document.querySelectorAll('.nav__item'))
 
+const colorScheme = document.querySelector('meta[name=color-scheme]');
+const switchButtons = document.querySelectorAll('.theme-switcher__button');
+
 const nested = document.querySelector('#menu-dropdown')
 const button = document.querySelector('.nav__button')
 
@@ -20,4 +23,17 @@ document.body.addEventListener('click', (e) => {
     } else if (e.target === button && !nested.classList.contains('open')) {
         nested.classList.add('open')
     }
+})
+
+switchButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		const currentButton = button
+
+		switchButtons.forEach((button) => button.setAttribute(
+				'aria-pressed', button === currentButton
+			)
+		)
+
+		colorScheme.content = button.value
+	})
 })
