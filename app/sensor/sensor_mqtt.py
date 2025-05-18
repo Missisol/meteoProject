ef96345cfa81f30dca0e239da14d79c7b6803cf2
Missisol <missisololol@gmail.com>
2025-05-19 01:17:34 +0300

    update compose

--- app/sensor/sensor_mqtt.py
+++ app/sensor/sensor_mqtt.py
@@ -1,23 +1,17 @@
 import paho.mqtt.client as mqtt
 from app import socketio
 
-broker_url = 'localhost'
+broker_url = '0.0.0.0'
 broker_port = 1883
 
 
-def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected success")
-            client.subscribe('/esp8266/bme280', qos=1)
-            client.subscribe('/esp8266/dht22', qos=1)
-        else:
-            print(f"Connected fail with code {rc}")
-
-    mqttc = mqtt.Client()
-    mqttc.on_connect = on_connect
-    mqttc.connect(broker_url, broker_port, 60)
-    return mqttc
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected success")
+        client.subscribe('/esp8266/bme280', qos=1)
+        client.subscribe('/esp8266/dht22', qos=1)
+    else:
+        print(f"Connected fail with code {rc}")
 
 
 def on_message(client, userdata, message):
@@ -25,10 +19,17 @@ def on_message(client, userdata, message):
     socketio.emit('other_message', message.payload)
 
 
+def connect_mqtt():
+    mqttc = mqtt.Client()
+    mqttc.on_connect = on_connect
+    mqttc.on_message = on_message
+    mqttc.connect_async(broker_url, broker_port, 60)
+    # mqttc.connect(broker_url, broker_port, 60)
+    return mqttc
+
+
 def run():
     mqttc = connect_mqtt()
-    mqttc.on_message = on_message
-
     mqttc.loop_start()
     return mqttc
 
